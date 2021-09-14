535c27e159624139f43c1798c340041cbcae8472
lynn94874 <69159123+lynn94874@users.noreply.github.com>
2021-09-14 23:21:42 +0800

    Update poisson_image_editing.cpp

--- poisson_image_editing.cpp
+++ poisson_image_editing.cpp
@@ -3,6 +3,10 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include "vector"
+#include "time.h"
+#include <Eigen/Dense>
+#include "Eigen/Sparse"
+#include "Eigen/IterativeLinearSolvers"
 
 #define ATD at<double>
 #define vector vector<Mat>
@@ -12,23 +16,30 @@
 
 using namespace cv;
 using namespace std;
+using namespace Eigen;
 
 i32 get_id(i32 i, i32 j, i32 width) {
 	return i * width + j;
 }
 // 求系数矩阵A
-Mat getA(i32 height, i32 width) {
-	Mat A = Mat::eye(height * width, height * width, CV_64FC1);
+SparseMatrix<float> getA(i32 height, i32 width) {
+	double s = clock();
+	i32 n = height * width;
+	Eigen::SparseMatrix<float> A(n, n);
+	for (i32 i = 0; i < n; i++)A.insert(i, i) = 1;
 	A *= -4;
 	for (i32 i = 0; i < height; i++) {
 		for (i32 j = 0; j < width; j++) {
 			i32 id = get_id(i, j, width);
-			if (i != 0) A.ATD(id, get_id(i - 1, j, width)) = 1;
-			if (i != height - 1) A.ATD(id, get_id(i + 1, j, width)) = 1;
-			if (j != 0) A.ATD(id, get_id(i, j - 1, width)) = 1;
-			if (j != width - 1) A.ATD(id, get_id(i, j + 1, width)) = 1;
+			if (i != 0) A.insert(id, get_id(i - 1, j, width)) = 1;
+			if (i != height - 1) A.insert(id, get_id(i + 1, j, width)) = 1;
+			if (j != 0) A.insert(id, get_id(i, j - 1, width)) = 1;
+			if (j != width - 1) A.insert(id, get_id(i, j + 1, width)) = 1;
 		}
 	}
+	A.makeCompressed();
+	double e = clock();
+	cout << e - s << endl;
 	return A;
 }
 // 求源图像散度
@@ -45,8 +56,8 @@ i32 get_div(Mat img, i32 i, i32 j, i32 roi_height, i32 roi_width) {
 	return x1 + x2 + y1 + y2 - 4 * img.ATD(i, j);
 }
 // 求b
-Mat getb(Mat img1, Mat img2, i32 roix, i32 roiy, i32 roi_height, i32 roi_width) {
-	Mat B = Mat::zeros(roi_height * roi_width, 1, CV_64FC1);
+VectorXf getb(Mat img1, Mat img2, i32 roix, i32 roiy, i32 roi_height, i32 roi_width) {
+	Eigen::VectorXf B(roi_height * roi_width, 1);
 	for (i32 i = 0; i < roi_height; i++) {
 		for (i32 j = 0; j < roi_width; j++) {
 			i32 div = get_div(img1, i, j, roi_height, roi_width);
@@ -55,36 +66,49 @@ Mat getb(Mat img1, Mat img2, i32 roix, i32 roiy, i32 roi_height, i32 roi_width) 
 			if (j == 0) div -= img2.ATD(roiy + i, roix + j - 1);
 			if (j == roi_width - 1) div -= img2.ATD(roiy + i, roix + j + 1);
 			i32 id = get_id(i, j, roi_width);
-			B.ATD(id, 0) = div;
+			B(id, 0) = div;
 		}
 	}
 	return B;
 }
 
-Mat get_result(Mat A, Mat b, i32 height) {
-	Mat result;
-	solve(A, b, result);
-	result = result.reshape(0, height);
-	return result;
+Mat get_result(SparseMatrix<float> A, VectorXf b, i32 roi_height, i32 roi_width) {
+	Eigen::VectorXf x;
+	Eigen::BiCGSTAB<Eigen::SparseMatrix<float>> Solver_sparse;
+	//Solver_sparse.setTolerance(0.00001);
+	double s = clock();
+	Solver_sparse.compute(A);
+	x = Solver_sparse.solve(b);
+	double e = clock();
+	cout << e - s << endl;
+	Mat t;
+	t = Mat::zeros(roi_height, roi_width, CV_64FC1);
+	for (i32 i = 0; i < roi_height; i++) {
+		for (i32 j = 0; j < roi_width; j++) {
+			t.ATD(i, j) = x(get_id(i, j, roi_width), 0);
+		}
+	}
+	return t;
 }
 
-Mat poisson_blending(i32 roix, i32 roiy, Mat img1, Mat img2, i32 roi_height, i32 roi_width, Mat A) {
+Mat poisson_blending(i32 roix, i32 roiy, Mat img1, Mat img2, i32 roi_height, i32 roi_width, SparseMatrix<float> A) {
 	vector rgb1, rgb2, result;
 	// 三个通道分别求解，最后再叠加
 	split(img1, rgb1);
 	split(img2, rgb2);
 
-	Mat b, res;
+	VectorXf b;
+	Mat res;
 	b = getb(rgb1[0], rgb2[0], roix, roiy, roi_height, roi_width);
-	res = get_result(A, b, roi_height);
+	res = get_result(A, b, roi_height, roi_width);
 	result.push_back(res);
 
 	b = getb(rgb1[1], rgb2[1], roix, roiy, roi_height, roi_width);
-	res = get_result(A, b, roi_height);
+	res = get_result(A, b, roi_height, roi_width);
 	result.push_back(res);
 
 	b = getb(rgb1[2], rgb2[2], roix, roiy, roi_height, roi_width);
-	res = get_result(A, b, roi_height);
+	res = get_result(A, b, roi_height, roi_width);
 	result.push_back(res);
 
 	Mat v;
@@ -95,27 +119,27 @@ Mat poisson_blending(i32 roix, i32 roiy, Mat img1, Mat img2, i32 roi_height, i32
 
 i32 main() {
 	Mat img1, img2;
-	Mat i1 = imread("C:/Users/lynn/Desktop/moon.png");
-	Mat i2 = imread("C:/Users/lynn/Desktop/cuhk.jfif");
+	Mat i1 = imread("C:/Users/lynn/Desktop/dog1.jpg");
+	Mat i2 = imread("C:/Users/lynn/Desktop/pool.jpg");
 	imshow("image1", i1);
 	imshow("image2", i2);
 	i1.convertTo(img1, CV_64FC3);
 	i2.convertTo(img2, CV_64FC3);
 
 	i32 roix, roiy, roi_height, roi_width;
-	roix = 200;
-	roiy = 50;
+	roix = 415;
+	roiy = 215;
 
 	roi_height = img1.rows;
 	roi_width = img1.cols;
 
-	Mat A = getA(roi_height, roi_width);
+	SparseMatrix<float> A = getA(roi_height, roi_width);
 	Mat Result = poisson_blending(roix, roiy, img1, img2, roi_height, roi_width, A);
 
 	Rect rc = Rect(roix, roiy, roi_width, roi_height);
 	Mat T = img2(rc);
 	Result.copyTo(T);
-	img2.convertTo(img2, CV_8UC1);
+	img2.convertTo(img2, CV_8UC3);
 	imshow("result", img2);
 
 	imwrite("C:/Users/lynn/Desktop/result.jpg", img2);
